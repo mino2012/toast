30b04aa1c75881649cbcd34f71ee41f730956c5a
florentL <florent.le.boulch@gmail.com>
2017-02-12 18:23:24 +0100

    Add google maps (angular ui)

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -44,7 +44,10 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/bootstrap-ui-datetime-picker/dist/datetime-picker.js',
             'src/main/webapp/bower_components/ng-file-upload/ng-file-upload.js',
             'src/main/webapp/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
+            'src/main/webapp/bower_components/angular-ui-utils/ui-utils.js',
+            'src/main/webapp/bower_components/angular-ui-event/dist/event.js',
             'src/main/webapp/bower_components/angular-mocks/angular-mocks.js',
+            'src/main/webapp/bower_components/angular-ui-map/ui-map.js',
             // endbower
             'src/main/webapp/app/app.module.js',
             'src/main/webapp/app/app.state.js',
